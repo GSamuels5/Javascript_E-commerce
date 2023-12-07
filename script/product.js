9eca3998628fa39bf85e6a525f069799064f6bab
GSamuels5 <ghamzahsamuels23@gmail.com>
2023-12-07 15:56:37 +0200

    changes added

--- script/product.js
+++ script/product.js
@@ -1,62 +1,61 @@
 document.querySelector('#footEdit').innerText = new Date().getUTCFullYear()
 // putting products in an array
-let products = JSON.parse(localStorage.getItem('products'))?
-JSON.parse(localStorage.getItem('products')) : localStorage.setItem('products',
-JSON.stringify(
-    [
-        {
-            'id': 1,
-            'make': 'crown',
-            'type': 'Antique',
-            'price': 4396.83,
-            'year': 1920,
-            'image' : 'https://i.postimg.cc/c1h1LxQ0/Antique-Crown-Watch-Co-pocket-as170a11992b.jpg'
-            
-        },
-        {
-            'id': 2,
-            'make': 'Seiko prestage',
-            'type': 'Mens',
-            'price': 20592,
-            'year': 1950,
-            'image' : 'https://i.postimg.cc/9f1ngM9x/image.jpg'
-        },
-        {
-            'id': 3,
-            'make': 'Rolex',
-            'type': 'Mens',
-            'price': 15000,
-            'year': 1963,
-            'image' : 'https://i.postimg.cc/3xZk65xS/Microsoft-Teams-image-10-1000x.jpg'
-        },
-        {
-            'id': 4,
-            'make': 'kiroh',
-            'type': 'Womens',
-            'price': 4500,
-            'year': 2003,
-            'image' : 'https://i.postimg.cc/htmkPcds/rtz5g-512.webp'
-        },
-        {
-            'id': 5,
-            'make': 'kiroh',
-            'type': 'Womens',
-            'price': 3500,
-            'year': 2001,
-            'image' : 'https://i.postimg.cc/N0HmzCZ5/71ng-D-Rf8-LL-SX679.jpg'
-        },
-    ]
-    ))
-    //display all products
-    let allProducts = document.querySelector('[data-everyItem]')
-    let cartProducts = []
+let products = JSON.parse(localStorage.getItem('products')) ?
+    JSON.parse(localStorage.getItem('products')) : localStorage.setItem('products',
+        JSON.stringify(
+            [{
+                    'id': 1,
+                    'make': 'crown',
+                    'type': 'Antique',
+                    'price': 4396.83,
+                    'year': 1920,
+                    'image': 'https://i.postimg.cc/c1h1LxQ0/Antique-Crown-Watch-Co-pocket-as170a11992b.jpg'
 
-    function displayProducts() {
+                },
+                {
+                    'id': 2,
+                    'make': 'Seiko prestage',
+                    'type': 'Mens',
+                    'price': 20592,
+                    'year': 1950,
+                    'image': 'https://i.postimg.cc/9f1ngM9x/image.jpg'
+                },
+                {
+                    'id': 3,
+                    'make': 'Rolex',
+                    'type': 'Mens',
+                    'price': 15000,
+                    'year': 1963,
+                    'image': 'https://i.postimg.cc/3xZk65xS/Microsoft-Teams-image-10-1000x.jpg'
+                },
+                {
+                    'id': 4,
+                    'make': 'kiroh',
+                    'type': 'Womens',
+                    'price': 4500,
+                    'year': 2003,
+                    'image': 'https://i.postimg.cc/htmkPcds/rtz5g-512.webp'
+                },
+                {
+                    'id': 5,
+                    'make': 'kiroh',
+                    'type': 'Womens',
+                    'price': 3500,
+                    'year': 2001,
+                    'image': 'https://i.postimg.cc/N0HmzCZ5/71ng-D-Rf8-LL-SX679.jpg'
+                },
+            ]
+        ))
+//display all products
+let allProducts = document.querySelector('[data-everyItem]')
+let cartProducts = JSON.parse(localStorage.getItem('cart')) || []
+
+function displayProducts() {
     allProducts.innerHTML = "";
-    if (products){
-    products.forEach((product, i) => {
-        allProducts.innerHTML +=
-        `<div class="card m-4" >
+    if (products) {
+        products.forEach((product) => {
+            allProducts.innerHTML +=
+                `<div class="card m-4" >
         <div class="card-header">
           <h5>${product.type} watches</h5>
         </div>
@@ -72,7 +71,7 @@ JSON.stringify(
     
           
         </div>`
-});
+        });
 
     }
 }
@@ -83,19 +82,19 @@ let inputSearch = document.querySelector('[search-product]')
 
 //addevent
 
-inputSearch.addEventListener('keyup',()=>{
-    try{
+inputSearch.addEventListener('keyup', () => {
+    try {
         let SearchItem = products.filter(
-            prod =>{
+            prod => {
                 return prod.type.toLowerCase().includes(inputSearch.value.toLowerCase())
             }
         )
         if (SearchItem) {
-            allProducts.innerHTML =""
+            allProducts.innerHTML = ""
             SearchItem.forEach(
                 item => {
                     allProducts.innerHTML +=
-                    `<div class="card m-4" >
+                        `<div class="card m-4" >
                     <div class="card-header">
                       <h5>${item.type} watches</h5>
                     </div>
@@ -111,57 +110,45 @@ inputSearch.addEventListener('keyup',()=>{
                 
                       
                     </div>`
-    
+
                 }
-    
+
             )
-        }else{
+        } else {
             allProducts.innerHTML = `<h1>Product not found </h1>`
         }
+    } catch (e) {
+        `product not found`
     }
-    catch(e){
-`product not found`    }
 
- } )
+})
 
-    
+
 //create sort button
- let sortBtn = document.querySelector('[data-sort]')
+let sortBtn = document.querySelector('[data-sort]')
 
 //create click event to sort items according to the prices
- sortBtn.addEventListener('click',
- function sortValues() {
-    let assendingOrder = true
-    products.sort((a,b)=>{
-        if (assendingOrder) {
-            return a.price - b.price
-        } else {
-            return b.price - a.price
-        }
+sortBtn.addEventListener('click',
+    function sortValues() {
+        let assendingOrder = true
+        products.sort((a, b) => {
+            if (assendingOrder) {
+                return a.price - b.price
+            } else {
+                return b.price - a.price
+            }
+        })
+        assendingOrder = !assendingOrder
+        displayProducts()
     })
-    assendingOrder = !assendingOrder
-    displayProducts()
- })
 
 //create an empty array for add to cart 
 //push empty array to local storage 
 //onclick add objects to new array 
 //create variable 
-function cartItems() {
-    let itemBtn = document.querySelectorAll('[data-cart]')
-    //add event click
-    itemBtn.forEach((btn, index)=>{
-
-        btn.addEventListener('click', 
-        ()=> {
-            let selectedItems = products[index]
-            //create  push to a new array
-            cartProducts.push(selectedItems)
-            //update local storage into empty array and show amount
-            localStorage.setItem('cart',JSON.stringify(cartProducts))
-            console.log(`Product ID ${selectedItems.id} added to cart.`);
-        })
+function cartItems(item) {
+    if(item) {
+        cartProducts.push(item)
+        localStorage.setItem('cart', JSON.stringify(cartProducts))
     }
-    )
 }
-cartItems()
